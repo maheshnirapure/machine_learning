a0eef46926da6dcdce1e6a962571c0a56e1becbd
Mahesh Nirapure <maheshnirapure2401@gmail.com>
2018-12-29 23:25:25 +0530

    machine_learning

---
+++ 1.py
@@ -0,0 +1,2 @@
+1.py
+print("mahesh nirapure class uit rgpv in it branch in uit rgpv so it the brain and train and devlop the data programin g language so it can be do somethin new in this projects so grade")
